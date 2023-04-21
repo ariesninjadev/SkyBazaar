30857ca84f29851330ce0e0d1399fa2eac923013
Äkwav <ekwav@coflnet.com>
2023-04-21 21:21:47 +0200

    change consumer group

--- Services/BazaarBackgroundService.cs
+++ Services/BazaarBackgroundService.cs
@@ -76,7 +76,7 @@ namespace Coflnet.Sky.SkyAuctionTracker.Services
                     }
                 }));
                 await service.CheckAggregation(session, bazaar);
-            }, stoppingToken, "sky-bazaar-test", 5);
+            }, stoppingToken, "sky-bazaar", 5);
         }
 
         private BazaarService GetService()
