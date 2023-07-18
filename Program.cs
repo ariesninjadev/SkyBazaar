4eafb8df7fe9972cb3aa45e3b79baf9504defcce
Äkwav <ekwav@coflnet.com>
2023-07-18 22:11:36 +0200

    remove dead code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Coflnet.Sky.SkyAuctionTracker.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -9,26 +8,6 @@ namespace Coflnet.Sky.SkyAuctionTracker
     {
         public static async Task Main(string[] args)
         {
-            /*var order = new dev.SellOrder()
-                            {
-                                Amount = 5,
-                                Orders = 2,
-                                PricePerUnit = 3.2
-                            };
-            await new BazaarService(null).AddEntry(new dev.BazaarPull(){
-                Timestamp = System.DateTime.Now,
-                Products = new System.Collections.Generic.List<dev.ProductInfo>()
-                {
-                    new dev.ProductInfo()
-                    {
-                        ProductId = "kevin",
-                        SellSummary = new System.Collections.Generic.List<dev.SellOrder>()
-                        {
-                            order,order,order,order,order,order,order,order,order,order
-                        }
-                    }
-                }
-            });*/
             CreateHostBuilder(args).Build().Run();
         }
 
