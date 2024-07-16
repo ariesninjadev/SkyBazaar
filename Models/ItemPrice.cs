3e1469fe33433e38907b90c482436fdc7eae0020
Aries Powvalla <apowvalla26@jesuitmail.org>
2024-07-16 13:38:02 -0700

    Update ItemPrice.cs

--- Models/ItemPrice.cs
+++ Models/ItemPrice.cs
@@ -5,5 +5,7 @@ namespace Coflnet.Sky.SkyBazaar.Models
         public string ProductId { get; set; }
         public double BuyPrice { get; set; }
         public double SellPrice { get; set; }
+        public double GreatestBuy { get; set; }
+        public double CheapestSell { get; set; }
     }
 }
